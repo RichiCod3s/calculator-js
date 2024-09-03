c8457f9916aaf52b6e61feef8529b840b261218a
RichiCod3s <108982619+RichiCod3s@users.noreply.github.com>
2024-09-03 23:29:55 +0800

    make operator functions

---
+++ calculator.js
@@ -0,0 +1,27 @@
+let firstNumber;
+let secondNumber;
+let operator;
+
+function add(numOne,numTwo){
+    return numOne+numTwo;
+}
+
+function subtract(numOne,numTwo){
+    return numOne - numTwo;
+}
+
+function multiply(numOne,numTwo){
+    return numOne*numTwo;
+} 
+
+function divide(numOne,numTwo){
+
+    if(numTwo == 0){
+        return numOne;
+    }
+    return numOne/numTwo;
+}
+
+function operate(operator, numOne, numTwo){
+
+} 
