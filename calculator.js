0dfb1768348a5aa45d2fd8bbc1092f18346ca70b
RichiCod3s <108982619+RichiCod3s@users.noreply.github.com>
2024-09-04 03:52:57 +0800

    refactor button functionality

--- calculator.js
+++ calculator.js
@@ -3,6 +3,7 @@ let secondNumber;
 let operator;
 const display = document.getElementById("display");
 const buttons = document.querySelectorAll('button');
+const functionButtons = document.querySelectorAll('.function-button');
 
 function appendToDisplay(input){
     display.value += input;
@@ -33,13 +34,27 @@ function operate(operator, numOne, numTwo){
 } 
 
 // buttons to display
-buttons.forEach(button => {
+
+   buttons.forEach(button => {
     button.addEventListener('click', () =>{
 
-        if(button.dataset.value == "AC"){
-            display.value ="";
-        }else{
-            display.value += button.dataset.value;
-        }
+        switch(button.dataset.value){
+            case "AC": 
+                display.value ="";
+                break;
+            case "+/-":
+                let calculation = display.value;
+                if (calculation.startsWith("-")) {
+                    // If the current value starts with a '-', remove it
+                    display.value = calculation.substring(1);
+                } else {
+                    // Otherwise, add a '-' at the beginning
+                    display.value = "-" + calculation;
+                }
+                break;
+                default:
+                    display.value += button.dataset.value;
+            
+        }//end switch
     })
    })
