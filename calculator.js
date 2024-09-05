19decad5dba6ce42ac9133bb7f3a26c45e2aba5e
RichiCod3s <108982619+RichiCod3s@users.noreply.github.com>
2024-09-05 21:08:05 +0800

    implement operatorButtons, numberButtons, equalsButtons, operations and calculator logic

--- calculator.js
+++ calculator.js
@@ -1,13 +1,18 @@
-let firstNumber = null;
-let secondNumber = null;
-let operator = null;
-let firstNumberInput = false;
+let firstNumber = null; // Stores the first number in the operation
+let secondNumber = null; // Stores the second number in the operation
+let operator = null; // Stores the current operator (+, -, *, /)
+let firstNumberInput = false; // Tracks whether the first number has been input
+let secondNumberInput = false; //// Tracks whether the second number is being input
+let calculated =false; // flag: number button pressed with a calculation, it resets the value.
 
 const display = document.getElementById("display");
 const numberButtons = document.querySelectorAll('.number, .bottom-button');
 const functionButtons = document.querySelectorAll('.function-button');
+const operatorButtons = document.querySelectorAll('.operator');
+const equals = document.querySelector("#equals");
+display.value = 0;
 
-
+//calculation functions
 function add(numOne,numTwo){
     return numOne+numTwo;
 }
@@ -29,33 +34,72 @@ function divide(numOne,numTwo){
 }
 
 function operate(operator, numOne, numTwo){
-
+    switch(operator){
+  
+        case "+":
+                display.value = add(numOne,numTwo);
+                break;
+        case "-":
+                display.value = subtract(numOne,numTwo)
+                break;
+        case "/":
+                display.value = divide(numOne,numTwo)
+                break;
+        case "*":
+                display.value = multiply(numOne, numTwo);
+                break;
+    }
+   
 } 
-     
+     // number buttons pressed - event listener
     numberButtons.forEach(button => {
         button.addEventListener('click', () =>{
-            // need to add . functionality
+
+            // press number with a calulation, the value is reset.
+            if(calculated || display.value == "0"){ 
+                display.value = '';
+            }
+
+            if(display.value == '.' || display.value == '0.' || display.value == '0'){
+                display.value = "0.";
+            }
+            
+
+            if(firstNumberInput == true && secondNumberInput == false){
+                display.value ="";
+                secondNumberInput = true;
+            }
+
             if(button.dataset.value == '.'){
-                display.value += button.dataset.value;
+                //decimal functionality
+                if(display.value == '0.' || display.value == 0){
+                    display.value = "0.";
+                }else{
+                    display.value += button.dataset.value;
+                }
             }else{
                 display.value += parseFloat(button.dataset.value);
             }
      
         })
+        
     });
 
-// buttons to display
-// one number to the display, do not display non numbers apart from . 
+
+    // function buttons clicked - event listener
    functionButtons.forEach(button => {
     button.addEventListener('click', () =>{
 
         switch(button.dataset.value){
-            case "AC": 
+            case "AC": // reset all
                 display.value ="";
                 firstNumber = null;
                 secondNumber =null
                 operator = null;
-                waitingForSecondNumber = false;
+                firstNumberInput = false;
+                secondNumberInput = false;
+                calculated = false;
+                display.value = 0;
                 break;
             case "+/-":
                 let currentValue = parseFloat(display.value);
@@ -64,9 +108,55 @@ function operate(operator, numOne, numTwo){
                 }
                 break;
             case "%":
-                // make this a number and perfom percentage
                 display.value = display.value/100;
                 break;
         }//end switch
     })
+   });
+
+   // when operator buttons are clicked - event listner
+   operatorButtons.forEach(button => {
+   button.addEventListener('click', () =>{
+    //first number is assigned once the operator is clicked
+    if(!firstNumberInput){
+        firstNumber = parseFloat(display.value);
+        firstNumberInput =true;  
+      }
+      
+      // sets operator 
+      switch(button.dataset.value){
+  
+          case "+":
+                  operator = "+";
+                  break;
+          case "-":
+                  operator = "-";
+                  break;
+          case "/":
+                  operator = "/";
+                  break;
+          case "*":
+                  operator = "*";
+                  break;
+      }
+
+      calculated =false;
+      // display half of the calculations - awaiting second number
+      display.value = display.value + operator;   
+   })
+   });
+
+   // equals button pressed - event listener
+   equals.addEventListener('click', () => {
+    if(firstNumberInput){
+       secondNumber =  parseFloat(display.value);
+       secondNumberInput = true;
+    }
+
+    if(firstNumberInput == true && secondNumberInput == true){
+         operate(operator, firstNumber, secondNumber);
+         firstNumberInput =false;
+         secondNumberInput=false;
+         calculated = true; // number pressed after sum - display is reset
+    }
    });
